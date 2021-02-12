a87d00c81a94b06dd908b0a2f2a972396ec1149b
Test <example@gmail.com>
2021-02-12 18:14:31 +0200

    First

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-const obj = {
-    name: "Alex",
-    age: "25",
-    isMarried: "false"
+let numberOfFilms, film1, reviews1, film2, reviews2;
+numberOfFilms = prompt("Сколько фильмов вы посомтрели");
+film1 = prompt("последний фильм");
+reviews1 = prompt("оценка");
+film2 = prompt("последний фильм");
+reviews2 = prompt("оценка");
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
 };
 
-console.log(obj);
+personalMovieDB.movies[film1] = reviews1;
+personalMovieDB.movies[film2] = reviews2;
 
-let arr = ['plum.png', 'test.png'];
+console.log(personalMovieDB);
